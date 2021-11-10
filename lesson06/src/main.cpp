6759e4c307d2f4ded91c907ffbd1c59deaf48558
Гриша <gregory239@mail.ru>
2021-11-10 07:47:56 +0300

    wq

--- lesson06/src/main.cpp
+++ lesson06/src/main.cpp
@@ -4,23 +4,42 @@
 
 #include "blur.h" // TODO реализуйте функцию блюра с произвольной силой размытия в файле blur.cpp
 
-void testSomeBlur() {
+void testSomeBlur(const std::string &name) {
+    cv::Mat img = cv::imread("lesson05/data/" + name + ".jpg");
+    std::string resultsDir = "lesson06/resultsData/";
+    if (!std::filesystem::exists(resultsDir)) { // если папка еще не создана
+        std::filesystem::create_directory(resultsDir); // то создаем ее
+    }
+    cv::Mat res = blur(img, 0.8);
+    cv::imwrite(resultsDir + name + "_grey.jpg", res);
     // TODO выберите любую картинку и любую силу сглаживания - проверьте что результат - чуть размытая картинка
     // Входные картинки для тестирования возьмите из предыдущего урока (т.е. по пути lesson05/data/*).
     // Результирующие картинки сохарняйте в эту папку (т.е. по пути lesson06/resultsData/*).
 }
 
-void testManySigmas() {
+void testManySigmas(const std::string &name) {
     // TODO возьмите ту же самую картинку но теперь в цикле проведите сглаживание для нескольких разных сигм
     // при этом результирующую картинку сохраняйте с указанием какая сигма использовалась:
     // для того чтобы в название файла добавить значение этой переменной -
     // воспользуйтесь функцией преобразующей числа в строчки - std::to_string(sigma)
+    cv::Mat img = cv::imread("lesson05/data/" + name + ".jpg");
+    std::string resultsDir = "lesson06/resultsData/";
+    if (!std::filesystem::exists(resultsDir)) { // если папка еще не создана
+        std::filesystem::create_directory(resultsDir); // то создаем ее
+    }
+    cv::Mat res;
+    for (int i = 0; i < 10; ++i) {
+        res = blur(img.clone(), (float)i/10);
+        cv::imwrite(resultsDir + name + "_" + std::to_string(i) + ".jpg", res);
+    }
+//    res = blur(img, 0.8);
+//    cv::imwrite(resultsDir + name + "_grey.jpg", res);
 }
 
 int main() {
     try {
-        testSomeBlur();
-        testManySigmas();
+//        testSomeBlur("line21_water_horizont");
+        testManySigmas("valve");
 
         return 0;
     } catch (const std::exception &e) {
@@ -28,4 +47,5 @@ int main() {
         return 1;
     }
 }
+
 
